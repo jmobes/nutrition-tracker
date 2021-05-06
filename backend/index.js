b141bf71a851565a3e16472eb4b2b6043ee1630e
Joseph Moberly <jmobes15@gmail.com>
2021-05-05 23:45:05 -0700

    added user file to model folder. This file will create the mongodb schema and model to structure mongodb user documents in the database.

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.DB_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("Connected to nutrition-tracker db..."))
+  .catch(() => console.log("Could not connect to database"));
