4d4c05ff0e53d636fd13177896e04791df25001b
Joseph Moberly <jmobes15@gmail.com>
2021-06-09 20:49:46 -0700

    added state to login component for controlled forms.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,19 +1,47 @@
+import { useState } from "react";
 import "./Login.css";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleChange = (e, id) => {
+    e.target.id === "email"
+      ? setEmail(e.target.value)
+      : setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form data was submitted");
+  };
+
   return (
     <section className="login">
-      <form className="login__form">
+      <form onSubmit={handleSubmit} className="login__form">
         <div className="login__email">
           <label htmlFor="email">Email</label>
-          <input type="text" id="email" />
+          <input onChange={handleChange} value={email} type="text" id="email" />
         </div>
         <div className="login__password">
           <label htmlFor="password">Password</label>
-          <input type="text" id="password" />
+          <input
+            onChange={handleChange}
+            value={password}
+            type="text"
+            id="password"
+          />
         </div>
-        <button className="login__submit">Log In</button>
+        <button type="submit" className="login__submit">
+          Log In
+        </button>
       </form>
+      <div className="login__extras">
+        <p className="forgot__password">Forgot password?</p>
+        <p className="login__signup">
+          Don't have an account? <span>Sign up now</span>
+        </p>
+      </div>
     </section>
   );
 };
