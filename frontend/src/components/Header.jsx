6433b95008e0f931801eabdac856d6b5f7e38bf8
Joseph Moberly <jmobes15@gmail.com>
2021-06-09 20:31:23 -0700

    created login form html and css

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,9 +3,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <header className="header">
-      <h1>
-        <span>C</span>OUNT I<span>T</span>
-      </h1>
+      <h1>Count It</h1>
     </header>
   );
 };
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,21 @@
 import "./Login.css";
 
 const Login = () => {
-  return <h1>LOGIN PAGE</h1>;
+  return (
+    <section className="login">
+      <form className="login__form">
+        <div className="login__email">
+          <label htmlFor="email">Email</label>
+          <input type="text" id="email" />
+        </div>
+        <div className="login__password">
+          <label htmlFor="password">Password</label>
+          <input type="text" id="password" />
+        </div>
+        <button className="login__submit">Log In</button>
+      </form>
+    </section>
+  );
 };
 
 export default Login;
