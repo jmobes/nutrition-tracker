2f03c39a10994e0f7b42aeaab9b0c4be7230f412
Joseph Moberly <jmobes15@gmail.com>
2021-06-12 13:45:24 -0700

    added a login and logout component that is rendered with the header.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,11 @@
 import "./Header.css";
+import LogInOut from "./LogInOut";
 
 const Header = () => {
   return (
     <header className="header">
       <h1>Count It</h1>
+      <LogInOut />
     </header>
   );
 };
---
+++ frontend/src/components/LogInOut.jsx
@@ -0,0 +1,14 @@
+import "./LogInOut.css";
+
+const LogInOut = () => {
+  return (
+    <nav className="logInOut">
+      <ul className="logInOut__nav">
+        <li className="logInOut__nav__item">Log In</li>
+        <li className="logInOut__nav__item">Sign Up</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default LogInOut;
