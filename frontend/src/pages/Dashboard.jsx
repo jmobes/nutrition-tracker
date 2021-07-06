dabe3f5977b3507232dab6b5a5a5f5444990d979
Joseph Moberly <jmobes15@gmail.com>
2021-07-06 02:11:12 -0700

    added dashboard container and header

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,13 @@
 import "./Dashboard.css";
 
 const Dashboard = () => {
-  return <section className="dashboard"></section>;
+  return (
+    <section className="dashboard">
+      <div className="dashboard__summary">
+        <h4 className="dashboard__summary__title">Your Daily Summary</h4>
+      </div>
+    </section>
+  );
 };
 
 export default Dashboard;
