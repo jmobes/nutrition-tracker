5e133bda00e725236e4a55973c7b55aef83ca2b1
Joseph Moberly <jmobes15@gmail.com>
2021-07-08 16:36:02 -0700

    created a form to take user input that will be used to calculate the user's tdee. Added styling for mobile phones

--- frontend/src/pages/Tdee.jsx
+++ frontend/src/pages/Tdee.jsx
@@ -15,12 +15,47 @@ const Tdee = () => {
         goals.
       </p>
       <form className="tdee__form">
-        <div className="tdee__form__gender">
+        <div className="tdee__form__gender form__caption">
           <strong>Gender</strong>
-          <label htmlFor="male">Male</label>
-          <input id="male" type="radio" name="gender" value="male" checked />
-          <label htmlFor="female">Female</label>
-          <input id="female" type="radio" name="gender" value="female" />
+          <div className="tdee__form__gender__genders">
+            <label htmlFor="male">Male</label>
+            <input id="male" type="radio" name="gender" value="male" checked />
+            <label htmlFor="female">Female</label>
+            <input id="female" type="radio" name="gender" value="female" />
+          </div>
+        </div>
+        <div className="tdee__form__age form__caption">
+          <strong>Age</strong>
+          <label>
+            <input type="number" />
+          </label>
+        </div>
+        <div className="tdee__form__weight form__caption">
+          <strong>Weight</strong>
+          <label>
+            <input type="number" />
+          </label>
+        </div>
+        <div className="tdee__form__height form__caption">
+          <strong>Height</strong>
+          <label>
+            <input type="number" placeholder="feet" />
+          </label>
+          <label>
+            <input type="number" placeholder="inches" />
+          </label>
+        </div>
+        <div className="tdee__form__activity">
+          <strong>Activity</strong>
+          <select name="activity">
+            <option value="sedentary" selected>
+              Sedentary (office job)
+            </option>
+            <option value="light">Light Exercise (1-2 days/week)</option>
+            <option value="moderate">Moderate Exercise (3-5 days/week)</option>
+            <option value="heavy">Heavy Exercise (6-7 days/week)</option>
+            <option value="athlete">Athlete (2x per day)</option>
+          </select>
         </div>
       </form>
     </div>
