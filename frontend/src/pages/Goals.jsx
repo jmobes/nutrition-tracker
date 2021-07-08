9127d54c4abfd00a7da76759dd274cf05bc340eb
Joseph Moberly <jmobes15@gmail.com>
2021-07-08 02:24:52 -0700

    finished html structure to goals page

--- frontend/src/pages/Goals.jsx
+++ frontend/src/pages/Goals.jsx
@@ -11,7 +11,6 @@ const Goals = () => {
       <p className="goals__tdee">
         Calculate your TDEE <a href="#">HERE</a>
       </p>
-
       <div className="goals__tables">
         <div className="goals__tables__calories">
           <div className="goals__tables__calories__header">
@@ -19,6 +18,116 @@ const Goals = () => {
               Daily Nutrition Goals
             </h3>
             <button className="goals__tables__button">Edit</button>
+          </div>
+          <div className="goals__tables__calories__row">
+            <p className="goals__tables__calories__key">
+              <strong>Calories</strong>
+            </p>
+            <p className="goals__tables__calories__value">1800</p>
+          </div>
+          <div className="goals__tables__calories__row">
+            <p className="goals__tables__calories__key">
+              <strong>Carbohydrates</strong> <span>225 g</span>
+            </p>
+            <p className="goals__tables__calories__value">50%</p>
+          </div>
+          <div className="goals__tables__calories__row">
+            <p className="goals__tables__calories__key">
+              <strong>Fat</strong> <span>60 g</span>
+            </p>
+            <p className="goals__tables__calories__value">10%</p>
+          </div>
+          <div className="goals__tables__calories__row">
+            <p className="goals__tables__calories__key">
+              <strong>Protein</strong> <span>90 g</span>
+            </p>
+            <p className="goals__tables__calories__value">40%</p>
+          </div>
+        </div>
+        <div className="goals__tables__micros">
+          <div className="goals__tables__micros__header">
+            <h3 className="goals__tables__micros__header__title">
+              Micronutrients
+            </h3>
+            <button className="goals__tables__button">Edit</button>
+          </div>
+          <div className="goals__tables__micros__row">
+            <p className="goals__tables__micros__key">
+              <strong>Saturated Fat</strong>
+            </p>
+            <p className="goals__tables__micros__value">21 g</p>
+          </div>
+          <div className="goals__tables__micros__row">
+            <p className="goals__tables__micros__key">
+              <strong>Polysaturated Fat</strong>
+            </p>
+            <p className="goals__tables__micros__value">0 g</p>
+          </div>
+          <div className="goals__tables__micros__row">
+            <p className="goals__tables__micros__key">
+              <strong>Monosaturated Fat</strong>
+            </p>
+            <p className="goals__tables__micros__value">0 g</p>
+          </div>
+          <div className="goals__tables__micros__row">
+            <p className="goals__tables__micros__key">
+              <strong>Trans Fat</strong>
+            </p>
+            <p className="goals__tables__micros__value">0 g</p>
+          </div>
+          <div className="goals__tables__micros__row">
+            <p className="goals__tables__micros__key">
+              <strong>Cholesterol</strong>
+            </p>
+            <p className="goals__tables__micros__value">300 mg</p>
+          </div>
+          <div className="goals__tables__micros__row">
+            <p className="goals__tables__micros__key">
+              <strong>Sodium</strong>
+            </p>
+            <p className="goals__tables__micros__value">2300 mg</p>
+          </div>
+          <div className="goals__tables__micros__row">
+            <p className="goals__tables__micros__key">
+              <strong>Potassium</strong>
+            </p>
+            <p className="goals__tables__micros__value">3500 mg</p>
+          </div>
+          <div className="goals__tables__micros__row">
+            <p className="goals__tables__micros__key">
+              <strong>Fiber</strong>
+            </p>
+            <p className="goals__tables__micros__value">38 g</p>
+          </div>
+          <div className="goals__tables__micros__row">
+            <p className="goals__tables__micros__key">
+              <strong>Sugar</strong>
+            </p>
+            <p className="goals__tables__micros__value">71 g</p>
+          </div>
+          <div className="goals__tables__micros__row">
+            <p className="goals__tables__micros__key">
+              <strong>Vitamin A</strong>
+            </p>
+            <p className="goals__tables__micros__value">100% Daily Value</p>
+          </div>
+          <div className="goals__tables__micros__row">
+            <p className="goals__tables__micros__key">
+              <strong>Vitamin C</strong>
+            </p>
+            <p className="goals__tables__micros__value">100% Daily Value</p>
+          </div>
+          <div className="goals__tables__micros__row">
+            <p className="goals__tables__micros__key">
+              <strong>Calcium</strong>
+            </p>
+            <p className="goals__tables__micros__value">100% Daily Value</p>
+          </div>
+          <div className="goals__tables__micros__row">
+            <p className="goals__tables__micros__key">
+              <strong>Iron</strong>
+            </p>
+            <p className="goals__tables__micros__value">100% Daily Value</p>
           </div>
         </div>
       </div>
