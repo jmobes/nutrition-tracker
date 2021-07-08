3d34e8208fc8d9d45cde173c80b9d271760717a4
Joseph Moberly <jmobes15@gmail.com>
2021-07-08 00:27:38 -0700

    created the html and styles for the tdee section of the goals page

--- frontend/src/pages/Goals.jsx
+++ frontend/src/pages/Goals.jsx
@@ -4,6 +4,24 @@ const Goals = () => {
   return (
     <section className="goals">
       <h2 className="goals__title">Your Fitness Goals</h2>
+      <p className="goals__description">
+        In order to achieve your goals, you need to know your{" "}
+        <strong>Total Daily Energy Expenditure</strong>.
+      </p>
+      <p className="goals__tdee">
+        Calculate your TDEE <a href="#">HERE</a>
+      </p>
+
+      <div className="goals__tables">
+        <div className="goals__tables__calories">
+          <div className="goals__tables__calories__header">
+            <h3 className="goals__tables__calories__header__title">
+              Daily Nutrition Goals
+            </h3>
+            <button className="goals__tables__button">Edit</button>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
