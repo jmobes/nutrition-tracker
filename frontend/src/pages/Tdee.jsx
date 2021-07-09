499dd37072de3e1c12814e33722c32ae1b77cea8
Joseph Moberly <jmobes15@gmail.com>
2021-07-08 17:34:08 -0700

    created the tdee results section and added styles

--- frontend/src/pages/Tdee.jsx
+++ frontend/src/pages/Tdee.jsx
@@ -58,6 +58,24 @@ const Tdee = () => {
           </select>
         </div>
       </form>
+      <div className="tdee__results">
+        <div className="tdee__results__maintenance">
+          <h3 className="tdee__results__maintenance__title">
+            Your Maintenance Calories
+          </h3>
+          <div className="tdee__results__maintenance__calories">
+            <p className="tdee__results__maintenance__cpd">
+              <strong>2,684</strong>
+              <span>calories per day</span>
+            </p>
+            <div className="divider"></div>
+            <p className="tdee__results__maintenance__cpw">
+              <strong>18,787</strong>
+              <span>calories per week</span>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
