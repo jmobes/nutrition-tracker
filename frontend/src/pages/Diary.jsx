31d24c32b21996bed71f56fd6613a3a7af2b5268
Joseph Moberly <jmobes15@gmail.com>
2021-07-11 19:37:41 -0700

    changed layout to make calendar icon appear before the date

--- frontend/src/pages/Diary.jsx
+++ frontend/src/pages/Diary.jsx
@@ -5,17 +5,24 @@ import "react-calendar/dist/Calendar.css";
 const Diary = () => {
   return (
     <section className="diary">
-      {/* <Calendar /> */}
       <h3 className="diary__title">Food Diary</h3>
+      <p className="diary__description">
+        Logging the food you eat is key to knowing how many calories you are
+        consuming. Make it a habit to track your calories and reach your long
+        term goals.
+      </p>
       <div className="diary__header">
+        <div className="diary__header__calendar__icon">
+          <i class="far fa-calendar-alt"></i>
+        </div>
         <div className="diary__header__calendar__buttons">
           <i class="diary__header__calendar__button fas fa-long-arrow-alt-left"></i>
           <p className="calendar__day">Thursday, July 8, 2021</p>
           <i class="diary__header__calendar__button fas fa-long-arrow-alt-right"></i>
         </div>
-        <div className="diary__header__calendar__icon">
-          <i class="far fa-calendar-alt"></i>
-        </div>
+        {/* <div className="diary__calendar">
+          <Calendar />
+        </div> */}
       </div>
     </section>
   );
