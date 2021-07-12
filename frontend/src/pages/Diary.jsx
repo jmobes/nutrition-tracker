5fb0b156432b1fc780d6ba827d8d07c5890657ac
Joseph Moberly <jmobes15@gmail.com>
2021-07-11 21:24:00 -0700

    styled the tables for each meal

--- frontend/src/pages/Diary.jsx
+++ frontend/src/pages/Diary.jsx
@@ -27,27 +27,159 @@ const Diary = () => {
       <div className="diary__meals">
         <div className="diary__meal">
           <h4 className="diary__meal__title">Breakfast</h4>
-          <div className="diary__meal__table">
-            <div className="diary__meal__table__header">
-              <p className="diary__meal__table__header__item">Name</p>
-              <p className="diary__meal__table__header__item">Calories</p>
-              <p className="diary__meal__table__header__item">Carbs</p>
-              <p className="diary__meal__table__header__item">Protein</p>
-              <p className="diary__meal__table__header__item">Fat</p>
-            </div>
-            <div className="diary__meal__table__food">
-              <div className="diary__meal__table__food__row">
-                <p className="diary__meal__table__food__row">
-                  Skippy Peanut Butter
-                </p>
-                <p className="diary__meal__table__food__row">190</p>
-                <p className="diary__meal__table__food__row">7</p>
-                <p className="diary__meal__table__food__row">7</p>
-                <p className="diary__meal__table__food__row">16</p>
-                <button>Delete</button>
-              </div>
-            </div>
-          </div>
+          <table className="diary__meal__table">
+            <thead>
+              <tr className="diary__meal__table__header">
+                <th>Name</th>
+                <th>Calories</th>
+                <th>Carbs</th>
+                <th>Protein</th>
+                <th>Fat</th>
+                <th className="empty__col"></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="diary__meal__table__row">
+                <td>Skippy Peanut Butter</td>
+                <td>190</td>
+                <td>7</td>
+                <td>7</td>
+                <td>16</td>
+                <td className="empty__col">-</td>
+              </tr>
+              <tr className="diary__meal__table__row">
+                <td>Celery</td>
+                <td>30</td>
+                <td>6</td>
+                <td>1</td>
+                <td>0</td>
+                <td className="empty__col">-</td>
+              </tr>
+              <tr className="diary__meal__table__row">
+                <td>Mashed Potatoes</td>
+                <td>300</td>
+                <td>50</td>
+                <td>10</td>
+                <td>8</td>
+                <td className="empty__col">-</td>
+              </tr>
+            </tbody>
+            <tfoot>
+              <tr className="diary__meal__table__footer">
+                <td>
+                  <button>Add Food</button>
+                </td>
+                <td>900</td>
+                <td>93</td>
+                <td>32</td>
+                <td>50</td>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
+        <div className="diary__meal">
+          <h4 className="diary__meal__title">Lunch</h4>
+          <table className="diary__meal__table">
+            <thead>
+              <tr className="diary__meal__table__header">
+                <th>Name</th>
+                <th>Calories</th>
+                <th>Carbs</th>
+                <th>Protein</th>
+                <th>Fat</th>
+                <th className="empty__col"></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="diary__meal__table__row">
+                <td>Skippy Peanut Butter</td>
+                <td>190</td>
+                <td>7</td>
+                <td>7</td>
+                <td>16</td>
+                <td className="empty__col">-</td>
+              </tr>
+              <tr className="diary__meal__table__row">
+                <td>Celery</td>
+                <td>30</td>
+                <td>6</td>
+                <td>1</td>
+                <td>0</td>
+                <td className="empty__col">-</td>
+              </tr>
+              <tr className="diary__meal__table__row">
+                <td>Mashed Potatoes</td>
+                <td>300</td>
+                <td>50</td>
+                <td>10</td>
+                <td>8</td>
+                <td className="empty__col">-</td>
+              </tr>
+            </tbody>
+            <tfoot>
+              <tr className="diary__meal__table__footer">
+                <td>
+                  <button>Add Food</button>
+                </td>
+                <td>900</td>
+                <td>93</td>
+                <td>32</td>
+                <td>50</td>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
+        <div className="diary__meal">
+          <h4 className="diary__meal__title">Dinner</h4>
+          <table className="diary__meal__table">
+            <thead>
+              <tr className="diary__meal__table__header">
+                <th>Name</th>
+                <th>Calories</th>
+                <th>Carbs</th>
+                <th>Protein</th>
+                <th>Fat</th>
+                <th className="empty__col"></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="diary__meal__table__row">
+                <td>Skippy Peanut Butter</td>
+                <td>190</td>
+                <td>7</td>
+                <td>7</td>
+                <td>16</td>
+                <td className="empty__col">-</td>
+              </tr>
+              <tr className="diary__meal__table__row">
+                <td>Celery</td>
+                <td>30</td>
+                <td>6</td>
+                <td>1</td>
+                <td>0</td>
+                <td className="empty__col">-</td>
+              </tr>
+              <tr className="diary__meal__table__row">
+                <td>Mashed Potatoes</td>
+                <td>300</td>
+                <td>50</td>
+                <td>10</td>
+                <td>8</td>
+                <td className="empty__col">-</td>
+              </tr>
+            </tbody>
+            <tfoot>
+              <tr className="diary__meal__table__footer">
+                <td>
+                  <button>Add Food</button>
+                </td>
+                <td>900</td>
+                <td>93</td>
+                <td>32</td>
+                <td>50</td>
+              </tr>
+            </tfoot>
+          </table>
         </div>
       </div>
     </section>
