2115e2c67c83d6fa2e7c31eb07d1382157db6305
Joseph Moberly <jmobes15@gmail.com>
2021-07-14 19:39:35 -0700

    added html and styles to the AddFood component. Created the input and matching foods container

--- frontend/src/pages/AddFood.jsx
+++ frontend/src/pages/AddFood.jsx
@@ -1,7 +1,53 @@
 import "./AddFood.css";
 
 const AddFood = () => {
-  return <h1>ADD FOOD PAGE!</h1>;
+  return (
+    <section className="add">
+      <h3 className="add__title">Add Food To Diary</h3>
+      <div className="add__input">
+        <strong>Search food by name</strong>
+        <label>
+          <input type="text" />
+        </label>
+        <button>Search</button>
+      </div>
+      <div className="add__matching">
+        <h4 className="add__matching__title">Matching Foods:</h4>
+        <div className="add__matching__foods">
+          <div className="add__matching__food">
+            <h5 className="add__matching__food__name">Steak</h5>
+            <p className="add__matching__food__description">
+              Deer Steak, 100g, 170 calories
+            </p>
+          </div>
+          <div className="add__matching__food">
+            <h5 className="add__matching__food__name">Steak</h5>
+            <p className="add__matching__food__description">
+              Elk Steak, 100g, 190 calories
+            </p>
+          </div>
+          <div className="add__matching__food">
+            <h5 className="add__matching__food__name">Steak</h5>
+            <p className="add__matching__food__description">
+              Ribeye Steak, 100g, 100 calories
+            </p>
+          </div>
+          <div className="add__matching__food">
+            <h5 className="add__matching__food__name">Steak</h5>
+            <p className="add__matching__food__description">
+              T-Bone Steak, 100g, 300 calories
+            </p>
+          </div>
+          <div className="add__matching__food">
+            <h5 className="add__matching__food__name">Steak</h5>
+            <p className="add__matching__food__description">
+              Sirloin Steak, 100g, 250 calories
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default AddFood;
