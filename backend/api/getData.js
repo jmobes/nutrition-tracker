ca5d875a21802293213cc3a0ea9d5cdcad223d43
Joseph Moberly <jmobes15@gmail.com>
2021-07-14 18:19:14 -0700

    added some query parameters to make the result easier to work with

--- backend/api/getData.js
+++ backend/api/getData.js
@@ -2,27 +2,29 @@ const fetch = require("node-fetch");
 require("dotenv").config();
 
 async function foodSearch(foodString) {
-  const food_search_url = `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${process.env.API_KEY}&query=${foodString}&dataType=Foundation&pageSize=25&sortBy=lowercaseDescription.keyword&sortOrder=asc`;
+  const food_search_url = `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${process.env.API_KEY}&query=${foodString}&dataType=Foundation,Survey%20%28FNDDS%29&pageSize=25&sortBy=lowercaseDescription.keyword&sortOrder=asc`;
   let list;
   try {
     const res = await fetch(food_search_url);
     list = await res.json();
-    console.log(list);
+    console.log(list.foods);
   } catch (err) {
     console.error(err);
   }
 }
 
 async function foodData(foodId) {
-  const food_data_url = `https://api.nal.usda.gov/fdc/v1/food/${foodId}?api_key=${process.env.API_KEY}`;
+  const food_data_url = `https://api.nal.usda.gov/fdc/v1/food/${foodId}?format=abridged&nutrients=203&nutrients=204&nutrients=205&nutrients=208&api_key=${process.env.API_KEY}`;
   let nutrients;
   try {
     const res = await fetch(food_data_url);
     nutrients = await res.json();
+    console.log(nutrients);
   } catch (err) {
     console.error(err);
   }
   return nutrients;
 }
 
-console.log(foodSearch("cheese"));
+// console.log(foodSearch("cheese"));
+console.log(foodData(747429));
