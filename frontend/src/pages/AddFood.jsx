d6b79b952a864a52599ccb0ad5bb1d7eb23d57f2
Joseph Moberly <jmobes15@gmail.com>
2021-07-15 02:40:45 -0700

    added html structure to the food item that the user selects

--- frontend/src/pages/AddFood.jsx
+++ frontend/src/pages/AddFood.jsx
@@ -46,6 +46,51 @@ const AddFood = () => {
           </div>
         </div>
       </div>
+      <div className="add__selected">
+        <h4 className="add__selected__name">Sirloin Steak</h4>
+        <div className="add__serving__size">
+          <strong>Serving Size: </strong>
+          <span>100g</span>
+        </div>
+        <div className="add__selected__macros">
+          <div className="add__selected__macro">
+            <strong>Calories</strong>
+            <p>250 calories</p>
+          </div>
+          <div className="add__selected__macro">
+            <strong>Carbs</strong>
+            <p>3g</p>
+          </div>
+          <div className="add__selected__macro">
+            <strong>Protein</strong>
+            <p>20g</p>
+          </div>
+          <div className="add__selected__macro">
+            <strong>Fat</strong>
+            <p>15g</p>
+          </div>
+        </div>
+        <form className="add__selected__form">
+          <div className="add__selected__form__servings">
+            <strong>How many servings?</strong>
+            <label>
+              <input type="number" value="1" />
+            </label>
+          </div>
+          <div className="add__selected__form__meal">
+            <strong>To which meal?</strong>
+            <select name="meal">
+              <option value="breakfast" selected>
+                Breakfast
+              </option>
+              <option value="lunch">Lunch</option>
+              <option value="dinner">Dinner</option>
+              <option value="snack">Snack</option>
+            </select>
+          </div>
+          <button>Add To Diary</button>
+        </form>
+      </div>
     </section>
   );
 };
