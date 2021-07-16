462e14229c12cdccd2923dbb3edb5fc138b223a4
Joseph Moberly <jmobes15@gmail.com>
2021-07-16 02:01:20 -0700

    added setMacros function that will take calories, carbs, protein, and fat macronutrients goals and update the user with those values

--- backend/controllers/goals.js
+++ backend/controllers/goals.js
@@ -13,6 +13,7 @@ const setWeight = async (req, res, next) => {
   if (error) {
     return next(new HttpError(error.details[0].message, 400));
   }
+
   const { currentWeight, goalWeight } = req.body;
   try {
     let user = await User.findById(userId);
@@ -23,7 +24,7 @@ const setWeight = async (req, res, next) => {
       user.goals.goalWeight = goalWeight;
     }
     await user.save();
-    res.json({ status: "success", user: user });
+    res.status(201).json({ status: "success", user: user });
   } catch (ex) {
     return next(new HttpError(ex.message, 500));
   }
@@ -38,17 +39,50 @@ function validateWeight(weight) {
   return schema.validate(weight);
 }
 
-const setMacros = async (req, res, next) => {};
+const setMacros = async (req, res, next) => {
+  const userId = req.params.uid;
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    return next(new HttpError("Invalid user id", 400));
+  }
+
+  const { error } = validateMacros(req.body);
+  if (error) {
+    return next(new HttpError(error.details[0].message, 400));
+  }
+
+  const { calories, carbs, protein, fat } = req.body;
+  try {
+    let user = await User.findById(userId);
+    if (calories) {
+      user.goals.calories = calories;
+    }
+    if (carbs) {
+      user.goals.carbs = carbs;
+    }
+    if (protein) {
+      user.goals.protein = protein;
+    }
+    if (fat) {
+      user.goals.fat = fat;
+    }
+
+    await user.save();
+    res.status(201).json({ status: "success", user: user });
+  } catch (ex) {
+    return next(new HttpError(ex.message, 500));
+  }
+};
 
 function validateMacros(macros) {
   let schema = Joi.object({
     calories: Joi.number().min(1200),
-    carbs: Joi.number().min(0),
-    protein: Joi.number().min(0),
-    fat: Joi.number().min(0),
+    carbs: Joi.number().min(0).max(100),
+    protein: Joi.number().min(0).max(100),
+    fat: Joi.number().min(0).max(100),
   });
 
   return schema.validate(macros);
 }
 
 module.exports.setWeight = setWeight;
+module.exports.setMacros = setMacros;
--- backend/routes/goals.js
+++ backend/routes/goals.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { setWeight } = require("../controllers/goals");
+const { setWeight, setMacros } = require("../controllers/goals");
 
 router.post("/weight/:uid", setWeight);
+router.put("/macros/:uid", setMacros);
 
 module.exports = router;
