09532c34992d8072299881963e2a987c393ede17
Joseph Moberly <jmobes15@gmail.com>
2021-07-17 15:47:22 -0700

    wrote validation to check if there is a valid userId on the createPost endpoint

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -3,7 +3,23 @@ const HttpError = require("../models/HttpError");
 const Joi = require("joi");
 const mongoose = require("mongoose");
 
-const createPost = () => {};
+const createPost = () => {
+  const userId = req.params.uid;
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    return next(new HttpError("Invalid user id", 400));
+  }
+
+  const { error } = validatePost(req.body);
+  if (error) {
+    return next(new HttpError(error.details[0].message, 400));
+  }
+
+  const { body } = req.body;
+  try {
+  } catch (ex) {
+    return next(new HttpError(ex.message, 500));
+  }
+};
 
 function validatePost() {}
 
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,4 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const { createPost } = require("../controllers/posts");
+
+router.post("/:uid", createPost);
 
 module.exports = router;
