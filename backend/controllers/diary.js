6164b14f23cc927a8371671233f8a5ea0c6e847d
Joseph Moberly <jmobes15@gmail.com>
2021-07-19 21:28:25 -0700

    created validateFood function to perform validation on the request body sent to the api endpoint

--- backend/controllers/diary.js
+++ backend/controllers/diary.js
@@ -5,6 +5,16 @@ const mongoose = require("mongoose");
 
 const addFood = async (req, res, next) => {};
 
-function validateFood(food) {}
+function validateFood(food) {
+  let schema = Joi.object({
+    name: Joi.string().min(1).required(),
+    calories: Joi.number().min(0).required(),
+    carbs: Joi.number().min(0).required(),
+    protein: Joi.number().min(0).required(),
+    fat: Joi.number().min(0).required(),
+  });
+
+  return schema.validate(food);
+}
 
 module.exports.addFood = addFood;
