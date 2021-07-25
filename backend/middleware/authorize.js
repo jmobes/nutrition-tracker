56731e101963a09cf8d01aa7142c47842f518493
Joseph Moberly <jmobes15@gmail.com>
2021-07-25 01:06:13 -0700

    created a middleware function that runs before the route logic is executed. The authorize function will check if the user sent a token in the request header and check if the token sent matches the user. The control will then be passed to the next middleware

---
+++ backend/middleware/authorize.js
@@ -0,0 +1,21 @@
+const HttpError = require("../models/HttpError");
+const jwt = require("jsonwebtoken");
+
+function auth(req, res, next) {
+  if (req.method === "OPTIONS") {
+    return next();
+  }
+  try {
+    const token = req.headers.token;
+    if (!token) return next(new ClientError("unauthorized", 401));
+
+    const verified = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+    req.user = verified.user;
+    next();
+  } catch (err) {
+    return next(new ClientError("unauthorized", 401));
+  }
+}
+
+module.exports = authorize;
