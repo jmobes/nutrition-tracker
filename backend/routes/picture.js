a05c3b69df863addb0303d25be294417a5b4a24f
Joseph Moberly <jmobes15@gmail.com>
2021-07-24 23:25:30 -0700

    finished implementation of getPicture function. The endpoint will send the url of the photo stored in the public/uploads folder on the server.

--- backend/routes/picture.js
+++ backend/routes/picture.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { uploadPicture, upload, getPicture } = require("../controllers/picture");
 
 router.post("/:uid", upload.single("profilePicture"), uploadPicture);
-router.get("/uid", getPicture);
+router.get("/:uid", getPicture);
 
 module.exports = router;
